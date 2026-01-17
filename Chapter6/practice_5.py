d4a037a0d4883b23f1ead4e01e3ed7a503b43feb
arpitasarjan <arusarjan@gmail.com>
2026-01-17 14:09:08 +0530

    Chapter 6 Conditional Expressions
    This repository covers the concept of Conditional Expressions in Python.
    It explains how decision-making works using if, if-else, and elif statements.
    The topic is demonstrated with clear syntax and simple examples.
    Conditional expressions help control the flow of a Python program.
    This content is beginner-friendly and easy to understand.

---
+++ Chapter6/practice_5.py
@@ -0,0 +1,8 @@
+list1 = ["harry", "rohan", "divya", "rahul"]
+name = input("Enter your name: ").lower()
+
+if name in list1:
+    print("Your name is in the list")
+else:
+    print("Your name is not in the list")
+
